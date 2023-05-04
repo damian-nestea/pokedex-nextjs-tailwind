e55f6d7b2d0a9a88edc943a40cde8b509462ff28
damian-nestea <damian@hotmail.cz>
2023-05-03 21:40:35 -0300

    feature criação de estrutura de páginas e arquivo data.js na pasta utils para navigationLinks

---
+++ pokedex-project/utils/data.js
@@ -0,0 +1,6 @@
+export const navigationLinks = [
+  { label: "Home", path: "/HomePage" },
+  { label: "Pokemon List", path: "/PokemonListPage" },
+  { label: "Pokedex", path: "/PokedexPage" },
+  { label: "About", path: "/AboutPage" },
+];
