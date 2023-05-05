59491f5434098301cc80e700685f92b2d87e4ff9
damian-nestea <damian@hotmail.cz>
2023-05-05 20:25:50 -0300

    feature adição e alinhamento de foto para fundo de seção na homepage

--- pokedex-project/src/pages/HomePage/index.js
+++ pokedex-project/src/pages/HomePage/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <>
+      <section className=" py-8 flex flex-col items-center">
+        <Image
+          src={"/../public/img/pokemonBackground.png"}
+          width={1000}
+          height={1000}
+          alt="Pokemon Wallpaper"
+        />
+      </section>
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
