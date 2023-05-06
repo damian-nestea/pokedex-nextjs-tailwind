895182d59eedb7647c0045938d330af1886cefb4
damian-nestea <damian@hotmail.cz>
2023-05-06 17:51:00 -0300

    feature alinhamento de fundo da primeira seção da home page e criação de botão para acessar lista de pokemons

--- pokedex-project/src/pages/HomePage/index.js
+++ pokedex-project/src/pages/HomePage/index.js
@@ -3,8 +3,10 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
-      <section className=" w-4/5 h-4/5 bg-pokemon-background-home bg-center py-8 flex flex-col items-center">
-        Pokemon
+      <section className=" w-full flex flex-col items-center">
+        <div className=" w-4/5 h-screen bg-pokemon-background-home bg-contain bg-no-repeat px-24 flex">
+          <button className=" px-8 py-4 bg-red-600 self-center text-white uppercase font-bold">Lista de Pokemons</button>
+        </div>
       </section>
     </>
   );
