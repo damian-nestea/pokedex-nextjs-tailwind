90478e460830ce77d5c8da914051b662ed396531
damian-nestea <damian@hotmail.cz>
2023-05-07 10:09:07 -0300

    feature estilização da seção com background de pokemon, borda e border-radius

--- pokedex-project/src/pages/HomePage/index.js
+++ pokedex-project/src/pages/HomePage/index.js
@@ -4,9 +4,9 @@ const HomePage = () => {
   return (
     <>
       <section className=" w-full flex flex-col items-center">
-        <div className=" w-4/5 h-screen bg-pokemon-background-home bg-contain bg-no-repeat px-24 flex">
-          <button className=" px-8 py-4 bg-intense-red self-center text-white uppercase font-bold">
-            Lista de Pokemons
+        <div className=" w-4/5 h-screen bg-pokemon-background-home bg-contain bg-no-repeat px-24 flex rounded-r-md border-2">
+          <button className=" px-8 py-4 bg-intense-red self-center text-white uppercase font-bold rounded-sm hover:opacity-80">
+            Pokemon List
           </button>
         </div>
       </section>
